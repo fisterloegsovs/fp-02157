de63fe99870870d1e6306d145b868cf8eea93686
Anders <anders.persson@live.dk>
2024-10-02 17:44:20 +0200

    Update 1-5.fsx

--- week1/1-5.fsx
+++ week1/1-5.fsx
@@ -1,9 +1,9 @@
 
-let fib n =
-  let rec fib_ n a b =
+let fibonacci n =
+  let rec fibonacci_ n a b =
     if n = 0 then a
-    else fib_ (n-1) b (a+b)
-  fib_ n 0 1
+    else fibonacci_ (n-1) b (a+b)
+  fibonacci_ n 0 1
 
-let result = [|for i in 0..10 -> fib i|]
+let result = [for i in 0..10 -> fibonacci i]
 printfn "%A" result
